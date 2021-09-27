168b05d28776780a1691cefcd0d4dec0e7383c94
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-27 05:17:21 -0500

    año

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@ end
 
 
 conexion.execute("INSERT INTO public.usuario 
-                 (nusuario, email, encrypted_password, password, 
-                  fechacreacion, created_at, updated_at, rol) 
-                 VALUES ('sip', 'sip@localhost', 
+ (nusuario, email, encrypted_password, password, 
+ fechacreacion, created_at, updated_at, rol) 
+ VALUES ('sip', 'sip@localhost', 
   '$2a$10$DjawRiYaz.1zY43Mi3vyPumgvgyiRtXcXdqJ5redFjb0YyB2ofVVm',
-                         '', '2014-08-14', '2014-08-14', '2014-08-14', 1);")
+  '', '2014-08-14', '2014-08-14', '2014-08-14', 1);")
