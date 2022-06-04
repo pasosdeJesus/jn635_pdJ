7eef9f054364ac5aa87547e5ea128175a723182a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-06-04 09:25:00 -0500

    Simplifica

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 conexion = ActiveRecord::Base.connection();
 
 # De motores
-Sip::carga_semillas_sql(conexion, 'sip', :datos)
-motor = ['heb412_gen', './']
+motor = ['sip', 'mr519_gen', 'heb412_gen', nil]
 motor.each do |m|
     Sip::carga_semillas_sql(conexion, m, :cambios)
     Sip::carga_semillas_sql(conexion, m, :datos)
@@ -11,7 +10,7 @@ end
 
 conexion.execute("INSERT INTO public.usuario 
  (nusuario, email, encrypted_password, password, 
- fechacreacion, created_at, updated_at, rol) 
+ fechacreacion, created_at, updated_at, rol, confirmed_at) 
  VALUES ('sip', 'sip@localhost', 
   '$2a$10$DjawRiYaz.1zY43Mi3vyPumgvgyiRtXcXdqJ5redFjb0YyB2ofVVm',
-  '', '2014-08-14', '2014-08-14', '2014-08-14', 1);")
+  '', '2014-08-14', '2014-08-14', '2014-08-14', 1, '2014-08-14');")
